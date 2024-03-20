96a2089129411d63d4d637c7bfa2024aa5d35e23
Borbos0 <nbmvc021@mail.ru>
2024-03-20 22:51:33 +0300

    complicated

--- vite-project/src/components/Main/Main.jsx
+++ vite-project/src/components/Main/Main.jsx
@@ -2,19 +2,25 @@ import InWork from '../Columns/InWorkColumn.jsx';
 import NeedToDo from '../Columns/NeedToDoColumn.jsx';
 import NotStatus from '../Columns/NonStatusColumn.jsx';
 import Test from '../Columns/TestingColumn.jsx';
-import Ready from '../Columns/ReadyColumn.jsx';
+import Column from '../Columns/MainColumn.jsx';
+import { card } from '../Data/Data.jsx' 
 
 export default function Main() {
+    
+    const testing = card.filter((c) => c.statusName === "Тестирование")
+    const inWork = card.filter((c) => c.statusName === "В работе")
+    console.log(inWork);
     return (
 	    <main class="main">
 		    <div class="container">
 			    <div class="main__block">
 				    <div class="main__content">
-                        <NotStatus />
+                        <Column array={testing}/>
+                        <Column array={inWork}/>
+                        {/* <InWork />
                         <NeedToDo />
-                        <InWork />
-                        <Test />
-                        <Ready />
+                        <NotStatus />
+                        <Test /> */}
                     </div>
                 </div>
             </div>
